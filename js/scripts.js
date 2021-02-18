14cc359efc7a8561359e162349c0158728a062f5
Elisabeth Appel <73082739+elisabethappel@users.noreply.github.com>
2021-02-18 17:46:31 -0500

    edit formatting, add descriptions

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpc2FiZXRoYXBwZWwiLCJhIjoiY2tsMTNnYTdmMmxhb
 
 var options = {
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+    style: 'mapbox://styles/mapbox/streets-v10', // style URL
     center: [34.526852,28.516690], // [lng, lat]
-    zoom: 12
+    zoom: 11.5
 }
 
 
@@ -15,60 +15,49 @@ map.addControl(nav, 'top-left');
 
 var divingData = [
   {
+    id: 'i',
     point: [34.5373, 28.5723],
-    name: 'Blue Hole',
-    title: 'ثقب الأزرق'
+    name: 'Blue Hole ثقب الأزرق',
+    description:'The Blue Hole has anabundance of coral, including star coral, gorgonians, soft coral, black coral and anemones (with their colorful clown fish). You can also observe some giant sponges (elephant ear) before exiting the water to the western part of the Blue Hole. The Blue Hole is one of Dahab’s most famous dive sites and should definitely not be missed. '
   },
   {
     point: [34.520931,28.509351],
-    name: 'Eel Garden',
-    title: 'البلو هول'
+    name: 'Eel Garden البلو هول',
+    description:'Eel Garden gets its name from the population of hundreds of Red Sea garden eels which inhabit the area. Eel garden is situated in Assalah bay, connected to the Lighthouse by a pedestrian walkway.'
   },
   {
     point: [34.522820,28.498335],
     name: 'Lighthouse',
-    title:''
+    description:'The dive brings you to the furthest part of the bay descending to a depth of 25 meters. The Western side has a sandy bottom with hard coral towers, whilst the Eastern side follows the edge of the reef undulating over and through cracks caused by the 1995 earthquake.'
   },
   {
     point: [34.519901,28.495468],
     name: 'Mashraba',
-    title:''
+    description:'Mashraba is home to a rich variety of fauna and flora. The sandy slope leads to a magnificent column of reef called Roman’s rock. Head south over some impressive formations before coming to a family of beautiful table coral at around 20 meters.'
   },
   {
     point: [34.513550,28.476602],
     name: 'Islands',
-    title:''
+    description:'A series of three hard coral pools with sandy bottoms are situated after the small reef edge. This area consists of massive pore corals populated by numerous species, including snappers, groupers, black spotted pufferfish, as well as a school of yellowtail barracuda.'
   },
   {
-    point: [34.503078,28.470414],
+    point: [34.507466,28.470271],
     name: 'Napoleon Reef',
-    title:'',
-    description: 'This site is situated a few hundred meters from the Islands by the sandy tongue that borders the north of the El-Qura bay. Napoleon Reef is best reached by boat or zodiac as it lies around 450 meters away from the beach. This huge madrepore tower is separated by a channel from the half-submerged reef: it comes up from a sandy seabed at around 20 meters depth and is frequented by plentiful fauna and some Napoleonfish (Cheilinus undulates) to whom this site owes its name.',
-    image: '<img src="https://redsea-diving.com/wp-content/uploads/2020/07/a1111.jpg">'
-  },
-  {
-    point: [34.505997,28.477055],
-    name: 'Lagoon',
-    title: 'ذا لاجون'
+    description: 'Napoleon Reef comes up from a sandy seabed at around 20 meters depth and is frequented by plentiful fauna and some napoleonfish (Cheilinus undulates) to whom this site owes its name.'
   }
-
 ]
-
-
 
 divingData.forEach(function(d) {
 
   var html =  `
     <div><h4>${d.name}</h4>
-    <h5>${d.title}</h5>
-    <i> ${d.description} </i></div>
-    ${d.image}
+    ${d.description} </div>
   `
 
-new mapboxgl.Marker({
+var marker = new mapboxgl.Marker({
       color: "orange"
   })
   .setLngLat(d.point)
   .setPopup(new mapboxgl.Popup().setHTML(html)) // add popup
-  .addTo(map);
+  .addTo(map)
 })
